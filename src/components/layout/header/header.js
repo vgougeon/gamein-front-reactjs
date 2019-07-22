b0cd62045c62026058530d3fe7c410b8fdce1172
VincentG <vincent.gougeon@ynov.com>
2019-07-23 01:11:18 +0200

    Header layout

--- src/components/layout/header/header.js
+++ src/components/layout/header/header.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './header.scss';
 
 class Header extends Component {
-    state = {  }
-    render() { 
-        return ( 
+    state = {}
+    render() {
+        return (
             <header>
-            </header>
-         );
+                <div class="item">
+                    <i class="fas fa-bars aside-action"></i>
+                    <div id="logo">
+                        <img src="assets/favicon.png" />
+                    </div>
+                    <div id="search">
+                        <i class="fas fa-search"></i>
+                        <input type="search" placeholder="Rechercher..." />
+                    </div>
+                    <div id="buttons">
+                        <a href="login"><button class="stroked-theme"><i class="fas fa-user-circle mr-1"></i>Connexion</button></a>
+                    </div>
+                </div>
+            </header >
+        );
     }
 }
- 
+
 export default Header;
