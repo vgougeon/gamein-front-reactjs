a8e2bc8db8c6524ec2ab9c7d313dda62608fb702
VincentG <vincent.gougeon@ynov.com>
2019-07-24 21:43:52 +0200

    Gamecard.js box view

--- src/components/shared/games/gamecard/gamecard.js
+++ src/components/shared/games/gamecard/gamecard.js
@@ -11,9 +11,9 @@ class GameCard extends Component {
         <div class="d-flex flex-column mx-2 m-g">
           <a href="">{this.props.name}</a>
           <span class="date">{this.props.release_year}</span>
-          <div class="d-flex">
+          <div class="d-flex flex-wrap">
           { this.props.console.map((console) =>
-            <span class="console s-1 mr-1" style={{background: console.background, color: console.color}}>
+            <span class="console s-1 mr-1 mb-1" style={{background: console.background, color: console.color}}>
               { console.short }
             </span>
           )}
--- src/components/views/games/games.page.js
+++ src/components/views/games/games.page.js
@@ -3,51 +3,65 @@ import axios from 'axios';
 import GameCard from "../../shared/games/gamecard/gamecard";
 import './games.page.scss';
 
-let state = { games: [], offset : 0 }
+let state = { games: [], offset: 0, view: '' }
 
 class GamesPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state = state;
 		this.appendGames = this.appendGames.bind(this);
+		this.viewBox = this.viewBox.bind(this);
 	}
 	componentDidMount() {
-		if(!this.state.games.length)
+		if (!this.state.games.length)
 			this.appendGames();
 	}
 	componentWillUnmount() {
-    state = this.state;
+		state = this.state;
 	}
 	getGames() {
-		return axios.get('http://api.njak.fr/getGames', { params: { offset: this.state.offset }});
+		return axios.get('http://api.njak.fr/getGames', { params: { offset: this.state.offset } });
 	}
-	appendGames(){
+	appendGames() {
 		let self = this
-		this.getGames().then( response => {
+		this.getGames().then(response => {
 			self.setState(state => ({
-				games : [...self.state.games, ...response.data],
-				offset : [...self.state.games, ...response.data].length 
+				games: [...self.state.games, ...response.data],
+				offset: [...self.state.games, ...response.data].length
 			}));
 		})
 	}
-	render() { 
-		return ( 
+	viewBox(){
+		if(this.state.view == ''){
+			this.setState(state => ({
+				view: 'view_box'
+			}));
+		}
+		else {
+			this.setState(state => ({
+				view: ''
+			}));
+		}
+	}
+	render() {
+		return (
 			<section id="page-content">
-        <div class="row mt-g g-g px-g">
-          <div class="col-xl-9 col-lg-12">
-						<div class="games">
-							{ this.state.games.map((game) =>
-								<GameCard { ...game} />
+				<button onClick={this.viewBox}><i class="fas fa-grip-horizontal"></i></button>
+				<div class="row mt-g g-g px-g">
+					<div class="col-xl-9 col-lg-12">
+						<div class={"games " + this.state.view}>
+							{this.state.games.map((game) =>
+								<GameCard {...game} />
 							)}
 						</div>
-          </div>
-          <div class="col-xl-3 d-none d-md-none d-sm-none d-lg-none d-xl-block">
+					</div>
+					<div class="col-xl-3 d-none d-md-none d-sm-none d-lg-none d-xl-block">
 						<button onClick={this.appendGames}>Test : Afficher Plus</button>
 					</div>
-        </div>
-      </section>
+				</div>
+			</section>
 		);
 	}
 }
- 
+
 export default GamesPage;
