61d6f1dca13ad0eedc125a86bacf1a09b40b865c
VincentG <vincent.gougeon@ynov.com>
2019-08-10 12:23:08 +0200

    Reset on success comment

--- src/components/shared/feed/comments/postComments/postComments.js
+++ src/components/shared/feed/comments/postComments/postComments.js
@@ -12,12 +12,15 @@ class PostComments extends Component {
         data.append("post_id", this.props.id);
         console.log(data)
         axios.post('http://54.37.228.12/api/newComment', data).then(res => {
-            this.props.newComment(res.data)
+            if(res.status === 200){
+                this.newCommentRef.reset();
+                this.props.newComment(res.data)
+            }
         })
     }
     render() {
         return(
-        <form onSubmit={ this.submitComment }>
+        <form onSubmit={ this.submitComment } ref={(el) => this.newCommentRef = el }>
             <div className="textarea-post m-g d-flex align-items-center">
                 <textarea className="p-g comment-textarea" name="content" placeholder="Commentez !" style={{overflow: 'hidden', overflowWrap: 'break-word', resize: 'none', height: '54px'}} />
                 <input type="submit" className="submit-comment ml-2" value="Envoyer"/>
