e15351d5463e666fddce624fe3cd921b36f245b8
VincentG <vincent.gougeon@ynov.com>
2019-08-10 19:43:02 +0200

    fix url

--- src/components/shared/feed/newPost/newPost.js
+++ src/components/shared/feed/newPost/newPost.js
@@ -15,7 +15,7 @@ class NewPost extends Component {
     const data = new FormData(e.target);
     this.newPostRef.reset();
 
-    axios.post('http://127.0.0.1:3001/newPost', data, 
+    axios.post('http://54.37.228.12/api/newPost', data, 
     {onUploadProgress(progressEvent){
       self.setState({
         isLoading: (progressEvent.loaded / progressEvent.total) * 100
