433bacfb8185d29d5d964e3a33246732d18621e7
VincentG <vincent.gougeon@ynov.com>
2019-08-14 21:22:49 +0200

    Send auth parameter

--- src/services/socket/openSocket.js
+++ src/services/socket/openSocket.js
@@ -1,4 +1,4 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('54.37.228.12:3000');
+const socket = openSocket('54.37.228.12:3000', { query: 'auth=' +localStorage.getItem('token')});
 
 export default socket;
