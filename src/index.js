9d8d4318219ab22c883e156267fc1a3ec1a9855b
VincentG <vincent.gougeon@ynov.com>
2019-08-17 12:54:09 +0200

    Fix path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./i18n";
-import socket from './services/listener/openSocket'
+import socket from './services/socket/openSocket'
 
 import './styles/animations.scss';
 import './styles/master.scss';
