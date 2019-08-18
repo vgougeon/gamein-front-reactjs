459f672bc30990085d0c9c886b535a0ef4fa130e
VincentG <vincent.gougeon@ynov.com>
2019-08-18 16:13:39 +0200

    Fix url

--- src/services/socket/openSocket.js
+++ src/services/socket/openSocket.js
@@ -1,4 +1,4 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('localhost:3000', { query: 'auth=' +localStorage.getItem('token')});
+const socket = openSocket('54.37.228.12:3000', { query: 'auth=' +localStorage.getItem('token')});
 
 export default socket;
