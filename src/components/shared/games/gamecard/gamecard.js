36097ba9a5eec0ad2164bb77f20f721d455f6621
VincentG <vincent.gougeon@ynov.com>
2019-08-24 18:48:56 +0200

    remove gradient

--- src/components/shared/games/gamecard/gamecard.js
+++ src/components/shared/games/gamecard/gamecard.js
@@ -6,7 +6,6 @@ class GameCard extends Component {
   render() {
     return (
       <div className="game-item s-1 mx-g2 mb-g">
-        <div className="bg-gradient"></div>
         { (this.props.skin && this.props.skin.length !== 0) ?
         <Img className="bg-game" src={`http://54.37.228.12/f/skin/${this.props.skin[0]}.jpg`} />
         : null
