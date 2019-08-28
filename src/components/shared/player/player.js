7fe60e12d67432d0ff5c78704c876cfbfbc96ec7
VincentG <vincent.gougeon@ynov.com>
2019-08-29 00:35:56 +0200

    Test player progress bar

--- src/components/shared/player/player.js
+++ src/components/shared/player/player.js
@@ -5,17 +5,47 @@ import Youtube from 'react-youtube';
 class Player extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  
+            duration: 0,
+            progress: 0,
+        }
+    }
+    getInfo = (event) => {
+        this.setState({
+            duration: event.target.getDuration(),
+            progress: 1
+        })
+        this.interval = setInterval(
+            () => {
+                this.setState(prevState => {
+                    return { progress: prevState.progress + 1 };
+                });
+            },
+            1000
+        )
+    }
+    resetInfo = (event) => {
+        clearInterval(this.interval)
+        this.setState({
+            progress: 0
+        })
     }
     render() { 
         return (
             <React.Fragment>
             { this.props.playing && this.props.videoId !== false &&
             <div id="player">
+                <div className="player-progress" style={{ width: '' + (this.state.progress / this.state.duration) * 100 + '%'}}>
+                </div>
+                <div className="video-container">
                 <Youtube videoId={ this.props.videoId} 
-                opts={{ height:'1', width:'1', playerVars:{
+                opts={{ height:'0', width:'0', playerVars:{
                     autoplay: 1
-                }}} />
+                }}} 
+                onPlay={this.getInfo}
+                onEnd={this.resetInfo}
+                />
+                </div> 
             </div>
             }
             </React.Fragment>
