a592ac5ed36ef5594c6f13583d5821f8b71566d8
VincentG <vincent.gougeon@ynov.com>
2019-09-01 12:11:39 +0200

    Player state & actions

--- src/components/shared/player/player.js
+++ src/components/shared/player/player.js
@@ -2,26 +2,53 @@ import React, { Component } from 'react';
 import './player.scss';
 import { connect } from 'react-redux';
 import Youtube from 'react-youtube';
+import Spinner from '../spinner/spinner-standard';
 class Player extends Component {
     constructor(props) {
         super(props);
         this.state = {  
             duration: 0,
             progress: 0,
+            videoId: false,
+            player: false,
+            status: -1
         }
     }
+    componentDidUpdate(prevProps, prevState) {
+        if(this.props.videoId !== this.state.videoId){
+            clearInterval(this.interval)
+            this.setState({
+                videoId: this.props.videoId,
+                progress: 0
+            })
+        }
+    }
+    getPlayer = (event) => {
+        this.setState({
+            player: event.target
+        })
+    }
+    updateState = (event) => {
+        console.log(event.data)
+        this.setState({
+            status: event.data
+        })
+    }
     getInfo = (event) => {
+        clearInterval(this.interval)
         this.setState({
             duration: event.target.getDuration(),
-            progress: 1
         })
+        this.timeStart()
+    }
+    timeStart = () => {
         this.interval = setInterval(
             () => {
                 this.setState(prevState => {
-                    return { progress: prevState.progress + 1 };
+                    return { progress: prevState.progress + .1 };
                 });
             },
-            1000
+            100
         )
     }
     resetInfo = (event) => {
@@ -30,24 +57,57 @@ class Player extends Component {
             progress: 0
         })
     }
+    pause = () => {
+        this.state.player.pauseVideo()
+        clearInterval(this.interval)
+    }
+    play = () => {
+        this.state.player.playVideo()
+        this.timeStart()
+    }
     render() { 
         return (
             <React.Fragment>
-            { this.props.playing && this.props.videoId !== false &&
-            <div id="player">
-                <div className="player-progress" style={{ width: '' + (this.state.progress / this.state.duration) * 100 + '%'}}>
-                </div>
+            <div id="player" className={ this.props.playing && this.props.videoId !== false ? 'show' : ''}>
+                
                 <div className="video-container">
                 <Youtube videoId={ this.props.videoId} 
                 opts={{ height:'0', width:'0', playerVars:{
                     autoplay: 1
-                }}} 
+                }}}
+                onReady={this.getPlayer}
                 onPlay={this.getInfo}
-                onEnd={this.resetInfo}
+                onStateChange={this.updateState}
                 />
-                </div> 
+                </div>
+
+                <div className="player-progress" 
+                style={{ width: '' + (this.state.progress / this.state.duration) * 100 + '%'}}>
+                </div>
+                
+                <div className="info">
+                    <div className="data"></div>
+                    <div className="actions">
+                        <i className="fas fa-random"/>
+                        <i className="fas fa-step-backward"/>
+                        <div className="middle-button">
+                            { this.state.status === 1 &&
+                            <i className="fas fa-pause" onClick={ this.pause }/>
+                            }
+                            { this.state.status === 2 &&
+                            <i className="fas fa-play" onClick={ this.play }/>
+                            }
+                            { (this.state.status === 3 || this.state.status === -1 || this.state.status === 0) &&
+                            <Spinner size='40'/>
+                            }
+                        </div>
+                        <i className="fas fa-step-forward"/>
+                        <i className="fas fa-redo-alt"/>
+                    </div>
+                    <div className="right"></div>
+                </div>
+                 
             </div>
-            }
             </React.Fragment>
         );
     }
