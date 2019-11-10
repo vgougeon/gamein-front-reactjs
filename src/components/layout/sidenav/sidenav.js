eef79820442115cfe9e04381c731051ba24f4a16
Vincent GOUGEON <gougeonvincent31@outlook.fr>
2019-11-10 10:51:27 +0100

    Multiple active links react router dom

--- src/components/layout/sidenav/sidenav.js
+++ src/components/layout/sidenav/sidenav.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import {withRouter} from 'react-router-dom';
 import { Trans } from 'react-i18next';
 import { UserContext } from '../../../services/auth/userProvider';
 import "./sidenav.scss";
@@ -9,22 +10,34 @@ class SideNav extends Component {
     return ((this.props.isOpen) ? "" : "aside-fold");
   }
   navClicked() {
+    // Cette fonction devra fermer le nav sur mobile lorsqu'un lien est cliqué
+  }
+  check = (paths) => {
+    for(const path of paths)
+      if(path == this.props.location.pathname.substr(1).split('/')[0])
+        return true
+    return false
   }
   render() {
     return (
         <aside className={this.isOpen()} >
           <div className="d-flex flex-column">
-            <NavLink exact={true} activeClassName='active' to='/' onClick={() => this.navClicked()}>
+            <NavLink isActive={() => { return this.check([''])}} activeClassName='active' to='/' onClick={() => this.navClicked()}>
               <div className="button">
                 <i className="fas fa-compass" /><Trans>nav-home</Trans>
               </div>
             </NavLink>
-            <NavLink exact={true} activeClassName='active' to='/games'>
+            <NavLink isActive={() => { return this.check(['games', 'game'])}} activeClassName='active' to='/games'>
               <div className="button">
                 <i className="fas fa-gamepad" /><Trans>nav-games</Trans>
               </div>
             </NavLink>
-            <NavLink exact={true} activeClassName='active' to='/party'>
+            {/* <NavLink exact={true} activeClassName='active' to='/party'>
+              <div className="button">
+                <i className="fas fa-chess-queen" /><Trans>nav-party</Trans>
+              </div>
+            </NavLink> */}
+            <NavLink isActive={() => { return this.check(['party'])}} activeClassName='active' to='/party'>
               <div className="button">
                 <i className="fas fa-chess-queen" /><Trans>nav-party</Trans>
               </div>
@@ -42,4 +55,4 @@ class SideNav extends Component {
   }
 }
 
-export default SideNav;
+export default withRouter(SideNav)
