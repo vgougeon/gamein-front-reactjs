dd92ebee6e21a490cc7eb2e1bc10605deb612aec
Vincent GOUGEON <gougeonvincent31@outlook.fr>
2019-11-14 14:10:03 +0100

    Theme refactor

--- src/components/shared/spinner/spinner-standard.js
+++ src/components/shared/spinner/spinner-standard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './spinner.scss';
 const Spinner = (props) => {
     return ( 
-        <div className="loader" style={{width: props.size, height: props.size, borderWidth: props.size/10}}></div>
+        <div className="loader" style={{width: props.size, height: props.size, borderWidth: props.size/7}}></div>
     );
 }
  
